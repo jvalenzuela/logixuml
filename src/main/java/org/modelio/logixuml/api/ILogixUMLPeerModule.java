d3c977965f4bdd45217d806c4659e2c88b6afc49
Jason Valenzuela <jvalenzuela1977@gmail.com>
2020-10-30 20:22:42 +0800

    Create initial baseline module.
    Per the module development tutorial, yielding a module that
    successfully builds and imports into Modelio.

---
+++ src/main/java/org/modelio/logixuml/api/ILogixUMLPeerModule.java
@@ -0,0 +1,10 @@
+package org.modelio.logixuml.api;
+
+import org.modelio.api.module.IPeerModule;
+
+/**
+ * @see com.modeliosoft.modelio.api.module.IPeerModule
+ */
+public interface ILogixUMLPeerModule extends IPeerModule {
+
+}
