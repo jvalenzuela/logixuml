a4d95718fc609e8778baae1485a0d0ba0561cacb
Jason Valenzuela <jvalenzuela1977@gmail.com>
2021-01-29 17:24:21 +0800

    Create state mapping class.

---
+++ src/main/java/org/modelio/logixuml/statemachineaoi/StateMap.java
@@ -0,0 +1,64 @@
+package org.modelio.logixuml.statemachineaoi;
+
+import static java.util.Collections.unmodifiableMap;
+import static java.util.Collections.unmodifiableSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.modelio.metamodel.uml.behavior.stateMachineModel.State;
+import org.modelio.vcore.smkernel.mapi.MObject;
+import org.modelio.vcore.smkernel.mapi.MRef;
+
+/**
+ * This class handles constructing a map of AoiState objects from all the states
+ * within the source state machine.
+ */
+class StateMap {
+    /**
+     * Builds a mapping of AoiState objects keyed by MRefs.
+     *
+     * @param elements Child model elements of the source state machine.
+     * @return Map of AoiState objects.
+     * @throws ExportException If a duplicate state name was found or a state was
+     *                         otherwise invalid.
+     */
+    static Map<MRef, AoiState> build(final Set<MObject> elements) throws ExportException {
+        final Set<String> stateNames = new HashSet<>();
+        final Map<MRef, AoiState> map = new HashMap<>();
+
+        for (final State stateElement : getStateElements(elements)) {
+            final AoiState aoiState = new AoiState(stateElement);
+
+            // Verify the name is unique among all other states. Names are treated in a
+            // case-insensitive manner because state names are used to create Logix tags,
+            // which are not case-sensitive.
+            final String normalizedName = aoiState.getName().toLowerCase();
+            if (stateNames.contains(normalizedName)) {
+                throw new ExportException("Duplicate state name.", stateElement);
+            }
+            stateNames.add(normalizedName);
+
+            map.put(new MRef(stateElement), aoiState);
+        }
+
+        return unmodifiableMap(map);
+    }
+
+    /**
+     * Extracts states from all the model elements of a state machine.
+     *
+     * @param elements Child model elements of the source state machine.
+     * @return The set of state model elements.
+     */
+    static private Set<State> getStateElements(final Set<MObject> elements) {
+        final Set<State> set = elements.stream() //
+                .filter(e -> e.getMClass().getQualifiedName().equals(State.MQNAME)) // Select only state objects
+                .map(e -> (State) e) // Cast to State object.
+                .collect(Collectors.toSet());
+        return unmodifiableSet(set);
+    }
+}
---
+++ src/test/java/org/modelio/logixuml/statemachineaoi/StateMapTests.java
@@ -0,0 +1,90 @@
+package org.modelio.logixuml.statemachineaoi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.modelio.metamodel.uml.behavior.stateMachineModel.Region;
+import org.modelio.metamodel.uml.behavior.stateMachineModel.State;
+import org.modelio.metamodel.uml.behavior.stateMachineModel.StateMachine;
+import org.modelio.vcore.model.CompositionGetter;
+import org.modelio.vcore.smkernel.mapi.MRef;
+
+/**
+ * Unit tests for the StateMap.build() method.
+ */
+class StateMapTests {
+    // Top-level mock model elements.
+    private StateMachine stateMachine;
+    private Region top;
+
+    @BeforeEach
+    void setUp() {
+        MockModule.init();
+        stateMachine = MockModel.stateMachine("sm", null);
+        top = MockModel.region(stateMachine);
+    }
+
+    /**
+     * Ensure valid states are included in the map.
+     */
+    @Test
+    void validState() {
+        final State superstate = MockModel.state("superstate", top);
+        final Region region = MockModel.region(superstate);
+        final State state = MockModel.state("state", region);
+        try {
+            final Map<MRef, AoiState> map = buildMap();
+            assertEquals(2, map.size());
+            assertTrue(map.containsKey(new MRef(superstate)));
+            assertTrue(map.containsKey(new MRef(state)));
+        } catch (ExportException e) {
+            fail();
+        }
+    }
+
+    /**
+     * Ensure states with duplicate names are rejected.
+     */
+    @ParameterizedTest
+    @ValueSource(strings = { "state", "STATE", "   State\r\n\t" })
+    void duplicateStateName(final String dupName) {
+        final State state = MockModel.state("state", top);
+        final State dup = MockModel.state(dupName, top);
+        final ExportException e = assertThrows(ExportException.class, () -> buildMap());
+        assertTrue((e.getModelObject() == state) || (e.getModelObject() == dup));
+    }
+
+    /**
+     * Ensure the result map is read-only.
+     */
+    @Test
+    void readOnly() {
+        try {
+            Map<MRef, AoiState> map = buildMap();
+            final State state = MockModel.state("state", top);
+            final MRef ref = new MRef(state);
+            assertThrows(UnsupportedOperationException.class, () -> map.put(ref, new AoiState(state)));
+        } catch (ExportException e) {
+            fail();
+        }
+    }
+
+    /**
+     * Builds a state map from the mock state machine.
+     *
+     * @return The built state map.
+     * @throws ExportException
+     */
+    private Map<MRef, AoiState> buildMap() throws ExportException {
+        return StateMap.build(CompositionGetter.getAllChildren(Arrays.asList(stateMachine)));
+    }
+}
